c45d6571c6e1872e9c670674a2dcdd9af6378c4a
Jody-septiawan <jody.septiawan5@gmail.com>
2022-10-28 02:41:01 +0700

    Widget

--- lib/src/widgets/header.dart
+++ lib/src/widgets/header.dart
@@ -8,7 +8,7 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: [Text('This is Header')]),
+      child: Text('This is Header'),
     );
   }
 }
